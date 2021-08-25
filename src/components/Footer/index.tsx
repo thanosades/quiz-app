1c77c51d7a3d367763e5861e1cf70841bdebbcc4
Thanos Dimitriades <thanosades@gmail.com>
2021-08-25 16:37:25 +0300

    add link in footer for opentdb

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,6 +3,7 @@ import './index.scss';
 export default function Footer() {
   return (
     <div className="footer">
+      <p>Powered by <a href="http://opentdb.com/">Open Trivia database</a></p>
       <p>Copyright {new Date().getUTCFullYear()}, Thanos Dimitriades</p>
     </div>
   );
