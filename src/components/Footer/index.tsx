c679a9639613c4d5fc8f94330651a62fc0b989df
Thanos Dimitriades <thanosades@gmail.com>
2021-09-21 16:49:21 +0300

    remove copyright

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>Powered by <a href="http://opentdb.com/">Open Trivia database</a></p>
-      <p>Copyright {new Date().getUTCFullYear()}, Thanos Dimitriades</p>
+      <p>Made by <a href="https://github.com/thanosades/">Thanos Dimitriades</a>, 2021</p>
     </footer>
   );
 }
