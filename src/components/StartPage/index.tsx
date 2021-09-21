ec3e80c31c5c2d08e38cfa6aee2975d392018c2d
Thanos Dimitriades <thanosades@gmail.com>
2021-09-21 15:33:54 +0300

    change basic button styles

--- src/components/StartPage/index.tsx
+++ src/components/StartPage/index.tsx
@@ -44,7 +44,7 @@ export default function StartPage({ startGame }: { startGame: (questions: Questi
       {loader && <div className='loaderContainer'><Loader /></div>}
       {!loader && (
         <div>
-          <h1>Open Trivia Quiz!</h1>
+          <h1>Ten Questions</h1>
           <form onSubmit={handleSubmit}>
             <label htmlFor="difficulty">Select difficulty</label>
             <select id='difficulty' value={difficulty} onChange={handleDifficultyChange}>
