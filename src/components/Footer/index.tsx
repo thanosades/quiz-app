c3101c93d72f1a6a7f054c6904e646f325f43b6e
Thanos Dimitriades <thanosades@gmail.com>
2021-11-15 14:48:30 +0200

    add portfolio link to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>Powered by <a href="http://opentdb.com/">Open Trivia database</a></p>
-      <p>Made by <a href="https://github.com/thanosades/">Thanos Dimitriades</a>, 2021</p>
+      <p>Made by <a href="http://thanosades.vercel.app/">Thanos Dimitriades</a>, 2021</p>
     </footer>
   );
 }
