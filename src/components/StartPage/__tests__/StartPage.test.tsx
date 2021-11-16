b4027763b68bf104bb0086f2fbdab407c05aeeda
Thanos Dimitriades <thanosades@gmail.com>
2021-11-16 23:06:53 +0200

    move StartPage test in component folder

--- src/components/StartPage/__tests__/StartPage.test.tsx
+++ src/components/StartPage/__tests__/StartPage.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
-import App from './App';
+import App from 'App';
 
 describe('Start page', () => {
   test('has dropdown menus', () => {
